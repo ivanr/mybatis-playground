4408e4ecf8fde3bec46399f7ffe5ae462ec66a0c
Ivan Ristic <ivanr@webkreator.com>
2019-07-18 10:56:53 +0100

    Always use explicit type handler mapping where possible.

--- src/main/com/webkreator/mybatis_playground/handlers/AuthorAsJson.java
+++ src/main/com/webkreator/mybatis_playground/handlers/AuthorAsJson.java
@@ -1,6 +1,8 @@
 package com.webkreator.mybatis_playground.handlers;
 
 import com.webkreator.mybatis_playground.Author;
+import org.apache.ibatis.type.MappedTypes;
 
+@MappedTypes(Author.class)
 public class AuthorAsJson extends AbstractJsonTypeHandler<Author> {
 }
