b172d488e835b51ffe63a7d25edcf8484878f16c
Ivan Ristic <ivanr@webkreator.com>
2019-07-18 11:06:41 +0100

    Simplify StringListAsJsonArray by inheriting from AbstractJsonTypeHandler.

--- src/main/com/webkreator/mybatis_playground/handlers/AbstractJsonTypeHandler.java
+++ src/main/com/webkreator/mybatis_playground/handlers/AbstractJsonTypeHandler.java
@@ -18,6 +18,11 @@ public abstract class AbstractJsonTypeHandler<T> extends BaseTypeHandler<T> {
     protected final static Gson gson = MybatisGson.instance();
 
     protected Type getType() {
+        // This class should be able to determine the correct
+        // type from the superclass using reflection. If it can't,
+        // override this method in the superclass and provide the
+        // correct TypeToken. For example:
+        // return new TypeToken<List<String>>(){}.getType();
         ParameterizedType pt = (ParameterizedType) getClass().getGenericSuperclass();
         return pt.getActualTypeArguments()[0];
     }
--- src/main/com/webkreator/mybatis_playground/handlers/StringListAsJsonArray.java
+++ src/main/com/webkreator/mybatis_playground/handlers/StringListAsJsonArray.java
@@ -1,47 +1,10 @@
 package com.webkreator.mybatis_playground.handlers;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import com.webkreator.mybatis_playground.DisableMybatisIntrospection;
-import com.webkreator.mybatis_playground.MybatisGson;
-import org.apache.ibatis.type.BaseTypeHandler;
-import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedTypes;
 
-import java.lang.reflect.Type;
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @MappedTypes(DisableMybatisIntrospection.class)
-public class StringListAsJsonArray extends BaseTypeHandler<List<String>> {
-
-    private List<String> fromJson(String json) {
-        Type listType = new TypeToken<List<String>>() {
-        }.getType();
-        return MybatisGson.instance().fromJson(json, listType);
-    }
-
-    @Override
-    public void setNonNullParameter(PreparedStatement ps, int i, List<String> list, JdbcType jdbcType) throws SQLException {
-        ps.setString(i, MybatisGson.instance().toJson(list));
-    }
-
-    @Override
-    public List<String> getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        return fromJson(rs.getString(columnName));
-    }
-
-    @Override
-    public List<String> getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        return fromJson(rs.getString(columnIndex));
-    }
-
-    @Override
-    public List<String> getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        return fromJson(cs.getString(columnIndex));
-    }
+public class StringListAsJsonArray extends AbstractJsonTypeHandler<List<String>> {
 }
